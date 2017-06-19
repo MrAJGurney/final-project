361c30f91251efc1f18530769e2924031d0e3e1e
Andrew Gurney <MrAJGurney@Gmail.com>
2017-06-19 12:01:47 +0100

    Shuffle functionality within canvas.js

--- client/src/canvas.js
+++ client/src/canvas.js
@@ -2,13 +2,19 @@ const THREE = require('../js/three');
 
 class Canvas {
   constructor(canvas) {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.canvas = canvas;
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.scene = new THREE.Scene();
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    canvas.appendChild(renderer.domElement);
+    this.AssignCanvas();
   }
+
+  AssignCanvas(canvas) {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.canvas.appendChild(renderer.domElement);
+  }
+
 }
 
 export default Canvas;
