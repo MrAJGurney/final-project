b6f9a134e936037aef613f1834052a7ef8f24dc9
Andrew Gurney <MrAJGurney@Gmail.com>
2017-06-19 11:17:21 +0100

    Intial commit with app skeleton

---
+++ client/src/app.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+window.onload = function() {
+  const appDiv = document.getElementById('app');
+  const header = document.createElement('h1');
+  header.textContent = "Hello World";
+  appDiv.appendChild(header);
+};
