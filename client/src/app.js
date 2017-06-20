5a00bba68367d687a5d1b3f51632c5e41f7384c8
Andrew Gurney <MrAJGurney@Gmail.com>
2017-06-20 13:00:17 +0100

    Moved generation looping outside the l_system

--- client/src/app.js
+++ client/src/app.js
@@ -17,10 +17,57 @@ function app() {
       '1': (['1', '1']),
       '0': (['1', '[', '0', ']', '0'])
     },
-    processing: (symbol) => {}
+    processing: (code) => {
+      const lines = [];
+      const stack = [];
+
+      let startCoord = {
+        x: 0,
+        y: 0
+      };
+      let angle = 0;
+
+      for (let character of code) {
+        switch (character) {
+          case '0':
+          case '1':
+            let direction = {
+              x: 1,
+              y: 0
+            };
+            let newDirection = {
+              x: (direction.x * Math.cos(angle * Math.PI / 180)) - (direction.y * Math.sin(angle * Math.PI / 180)),
+              y: (direction.x * Math.sin(angle * Math.PI / 180)) + (direction.y * Math.cos(angle * Math.PI / 180))
+            };
+            let endCoord = {
+              x: startCoord.x + newDirection.x,
+              y: startCoord.y + newDirection.y
+            };
+            lines.push({start: startCoord, end: endCoord});
+            startCoord = endCoord;
+            break;
+          case '[':
+            stack.push({startCoord: startCoord, angle: angle});
+            angle -= 45;
+            break;
+          case ']':
+            let stackReturn = stack.pop();
+            startCoord = stackReturn.startCoord;
+            angle = stackReturn.angle;
+            angle += 45;
+            break;
+          default:
+            console.log('NO RULE ASSISGNED TO CHARACTER:', character);
+        }
+      }
+
+      return lines;
+    }
   });
 
-  fractalBinaryTree.GetCode(5);
+  [...Array(5)].forEach(() => {
+    fractalBinaryTree.GenerateCode()
+  });
   fractalBinaryTree.ProcessCode().forEach((line) => {
     let lineStart = new THREE.Vector3(0, line.start.x, line.start.y);
     let lineEnd = new THREE.Vector3(0, line.end.x, line.end.y);
