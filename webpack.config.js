0adc048acf7d7af2c90f0c292b7176e921c13f50
Andrew Gurney <AGurney@FindMyPast.com>
2019-04-29 19:07:25 +0100

    Update all packages to latest Updated all packages to latest (to prepare repository for a test framework), and then fixed any issues that arose.
    In addition, removed packages that were previously included as peer
    dependancies, but have now been replaced.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,23 @@
-var config = {
+const config = {
   entry: './app/index.js',
   output: {
     path: __dirname + '/build',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   resolve: {
-    extensions: ['.js']
+    extensions: ['.js'],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  }
+      },
+    ],
+  },
+  mode: `development`,
 };
 
 module.exports = config;
